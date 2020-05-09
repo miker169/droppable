77a538949a8009e39ac87f7fe0a75d4abca70729
Mike <mike@Helens-iMac.local>
2020-05-09 18:41:28 +0100

    Moved everything out into modules

---
+++ src/components/base-component.ts
@@ -0,0 +1,40 @@
+
+export default abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+  
+  protected constructor(
+    templateID: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string,
+  ) {
+    this.templateElement = document.getElementById(
+      templateID,
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+    
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+    
+    this.attach(insertAtStart);
+  }
+  
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? 'afterbegin' : 'beforeend',
+      this.element,
+    );
+  }
+  
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
---
+++ src/components/project-list.ts
@@ -0,0 +1,73 @@
+import { Project, ProjectStatus } from '../models/project.js';
+import { ProjectItem } from './project-item.js';
+import { DragTarget } from '../models/drag-drop.js';
+import Component from './base-component.js';
+import { AutoBind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
+
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
+  assignedProjects: Project[];
+  
+  constructor(private type: 'active' | 'finished') {
+    super('project-list', 'app', false, `${type}-projects`);
+    this.assignedProjects = [];
+    
+    this.configure();
+    this.renderContent();
+  }
+  @AutoBind
+  dragOverHandler(event: DragEvent) {
+    debugger;
+    if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain'){
+      event.preventDefault();
+      const listEl = this.element.querySelector('ul') as HTMLUListElement;
+      listEl.classList.add('droppable');
+    }
+  }
+  
+  @AutoBind
+  dropHandler(event: DragEvent) {
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
+  }
+  @AutoBind
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul') as HTMLUListElement;
+    listEl.classList.remove('droppable');
+  }
+  
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`,
+    ) as HTMLUListElement;
+    listEl.innerHTML = '';
+    for (const prjItem of this.assignedProjects) {
+      new ProjectItem(listEl.id, prjItem);
+    }
+  }
+  
+  configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler);
+    this.element.addEventListener('dragleave', this.dragLeaveHandler);
+    this.element.addEventListener('drop', this.dropHandler);
+    projectState.addListener((projects: Project[]) => {
+      this.assignedProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        } else {
+          return prj.status === ProjectStatus.Finished;
+        }
+      });
+      this.renderProjects();
+    });
+  }
+  
+  renderContent() {
+    let listId: string;
+    listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + ' PROJECTS';
+  }
+}
+
